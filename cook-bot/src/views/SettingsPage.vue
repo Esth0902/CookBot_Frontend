caec97f95e102d4e2e02832e78c78c8be851e567
Esth0902 <esther.stassin@gmail.com>
2025-11-11 09:18:48 +0100

    Cookbot initial commit - utilisation de la caméra pour prendre une photo

---
+++ cook-bot/src/views/SettingsPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Settings</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Configuration</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list>
+        <ion-item>
+          <ion-toggle :enable-on-off-labels="true">Enable Notifications</ion-toggle>
+        </ion-item>
+        <ion-item>
+          <ion-toggle>Receive Emails</ion-toggle>
+        </ion-item>
+        <ion-item>
+          <ion-toggle>Receive Text Messages</ion-toggle>
+        </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+</script>
---
+++ cook-bot/src/views/TabsPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/tabs/home">
+          <ion-icon aria-hidden="true" :icon="home" />
+          <ion-label>Accueil</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="myfridge" href="/tabs/myFridge">
+          <ion-icon :icon="camera" />
+          <ion-label>Mon Frigo</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="settings" href="/tabs/settings">
+          <ion-icon aria-hidden="true" :icon="settings" />
+          <ion-label>Réglages</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { home, camera, settings } from 'ionicons/icons';
+</script>
