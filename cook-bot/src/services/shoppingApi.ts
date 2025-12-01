af47875b8f2b51943bedfe4b1ce2f245d5e66578
LaetitiaV <laetitia.voue@gmail.com>
2025-12-01 22:22:09 +0100

    DébutPageShopping Et Services

---
+++ cook-bot/src/services/shoppingApi.ts
@@ -0,0 +1,96 @@
+import { getToken } from './authApi';
+
+const API_BASE_URL = 'http://localhost:8080';
+
+export interface ShoppingItem {
+    id?: number;
+    name: string;
+    checked: boolean;
+    quantity?: string;
+}
+
+export interface ShoppingList {
+    id?: number;
+    shoppingListName: string;
+    creationDate?: string;
+    items: ShoppingItem[];
+}
+
+export async function getAllShoppingLists(): Promise<ShoppingList[]> {
+    const token = getToken();
+    const response = await fetch('/api/v1/shopping/', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Erreur lors de la récupération des listes');
+    }
+
+    const result = await response.json();
+    return result.data;
+}
+
+export async function addItemToList(listId: number, itemName: string): Promise<ShoppingList> {
+    const token = getToken();
+
+    const response = await fetch(`${API_BASE_URL}/api/v1/shopping/item/${listId}`, {
+    method: 'POST',
+    headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: itemName,
+        checked: false
+    }),
+});
+    if (!response.ok) {
+        throw new Error('Erreur lors de l\'ajout de l\'item');
+    }
+
+    const result = await response.json();
+    return result.data;
+}
+
+export async function deleteShoppingList(listId: number): Promise<void> {
+    const token = getToken();
+
+    const response = await fetch(`${API_BASE_URL}/api/v1/shopping/${listId}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error('Erreur lors de la suppression');
+    }
+}
+
+export async function createShoppingList(name: string): Promise<ShoppingList> {
+    const token = getToken();
+
+    const response = await fetch(`${API_BASE_URL}/api/v1/shopping`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            shoppingListName: name,
+            items: []
+        }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Erreur lors de la création de la liste");
+    }
+
+    const result = await response.json();
+    return result.data;
+}
--- cook-bot/src/views/ShoppingPage.vue
+++ cook-bot/src/views/ShoppingPage.vue
@@ -1,14 +1,175 @@
 <template>
   <ion-page>
     <Header />
-Mes courses
+
+<ion-content class="shopping-content">
+
+  <h1 class="shopping-title">Mes listes de courses</h1>
+
+  <div v-if="!shoppingLists.length" class="shopping-empty">
+    <p>Aucune liste de courses pour le moment</p>
+    <IonButton class="add-list-btn" @click="handleAddList">
+    + Ajouter une liste
+  </IonButton>
+  </div>
+
+
+
+  <div v-else>
+
+    <div v-for="list in shoppingLists" :key="list.id"
+       class="shopping-list">
+
+    <IonInput
+        v-model="list.shoppingListName"
+        class="shopping-list-title"
+        placeholder="Titre de la liste"
+    />
+
+
+    <IonButton fill="clear"
+               class="shopping-add-btn">
+      + Ajouter un élément
+    </IonButton>
+
+    <IonReorderGroup :disabled="false">
+      <IonItem
+          v-for="item in list.items"
+          :key="item.id"
+          class="shopping-list-item"
+      >
+        <IonCheckbox
+            :checked="item.checked"
+            class="checkbox"
+        ></IonCheckbox>
+
+        <IonLabel :class="{ checked: item.checked }">
+          {{ item.name }}
+        </IonLabel>
+
+        <IonReorder slot="end"></IonReorder>
+      </IonItem>
+    </IonReorderGroup>
+
+      <p v-if="!list.items?.length" class="shopping-empty-list">
+        Cette liste est vide
+      </p>
+    </div>
+  </div>
+
+</ion-content >
+
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import {
-  IonPage,
+    IonPage,
+    IonContent,
+    IonInput,
+    IonItem,
+    IonLabel,
+    IonButton,
+    IonReorder,
+    IonReorderGroup,
+    IonCheckbox
 
 } from '@ionic/vue';
+
 import Header from "@/components/Header.vue";
-</script>
+import {ref} from 'vue';
+import {onIonViewWillEnter} from "@ionic/vue";
+import {getAllShoppingLists, ShoppingList, createShoppingList} from "@/services/shoppingApi";
+
+const shoppingLists = ref<ShoppingList[]>([])
+const loading = ref(true)
+
+onIonViewWillEnter(async () => {
+  loading.value = true;
+  try {
+    shoppingLists.value = await getAllShoppingLists();
+    console.log('Liste chargée:', shoppingLists.value);
+  }
+  catch (error) {
+    console.error('Erreur:', error);
+  }
+  finally {
+    loading.value = false;
+  }
+    });
+
+async function handleAddList() {
+  const name = prompt("Nom de la nouvelle liste :");
+
+  if (!name || name.trim() === "") {
+    return;
+  }
+
+  try {
+    const newList = await createShoppingList(name);
+    shoppingLists.value.push(newList);
+  } catch (error) {
+    console.error("Erreur création :", error);
+  }
+}
+
+
+  </script>
+
+<style scoped>
+
+.add-list-btn {
+  margin-bottom: 20px;
+  --color: var(--ion-color-secondary);
+}
+
+.shopping-content {
+  padding: 22px;
+}
+
+.shopping-list {
+  background: rgba(255, 255, 255, 0.05);
+  padding: 20px;
+  border-radius: 16px;
+  backdrop-filter: blur(6px);
+  border: 1px solid var(--ion-color-primary);
+  margin-bottom: 24px;
+  transition: 0.3s;
+}
+
+.shopping-list:hover {
+  transform: translateY(-4px);
+}
+
+.shopping-list-title {
+  margin-bottom: 14px;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: var(--ion-color-light);
+}
+
+.shopping-add-btn {
+  margin-bottom: 16px;
+  --color: var(--ion-color-secondary);
+}
+
+.shopping-list-item {
+  --background: transparent;
+  border-bottom: 1px solid var(--ion-color-primary);
+  padding: 10px 4px;
+}
+
+.shopping-list-item:last-child {
+  border-bottom: none;
+}
+
+.checkbox {
+  margin-right: 12px;
+}
+
+.checked {
+  text-decoration: line-through;
+  opacity: 0.6;
+}
+
+</style>
